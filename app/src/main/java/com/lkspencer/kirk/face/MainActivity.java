570c4c561f410528ff216c794ca9ad502014b156
Kirk Spencer <kirk@lkspencer.com>
2015-06-04 12:37:05 -0700

    Setup activity so that it can access the camera through the opencv library.

--- app/src/main/java/com/lkspencer/kirk/face/MainActivity.java
+++ app/src/main/java/com/lkspencer/kirk/face/MainActivity.java
@@ -2,15 +2,63 @@ package com.lkspencer.kirk.face;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
+
+  private final String TAG = "asdf";
+  private CameraBridgeViewBase mOpenCvCameraView;
+
+  private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    @Override
+    public void onManagerConnected(int status) {
+      switch (status) {
+        case LoaderCallbackInterface.SUCCESS:
+        {
+          Log.i(TAG, "OpenCV loaded successfully");
+          mOpenCvCameraView.enableView();
+        } break;
+        default:
+        {
+          super.onManagerConnected(status);
+        } break;
+      }
+    }
+  };
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    Log.i(TAG, "called onCreate");
     super.onCreate(savedInstanceState);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     setContentView(R.layout.activity_main);
+    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+    mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+    mOpenCvCameraView.setCvCameraViewListener(this);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (mOpenCvCameraView != null)
+      mOpenCvCameraView.disableView();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
   }
 
   @Override
@@ -34,4 +82,26 @@ public class MainActivity extends AppCompatActivity {
 
     return super.onOptionsItemSelected(item);
   }
+
+  @Override
+  public void onCameraViewStarted(int width, int height) {
+
+  }
+
+  @Override
+  public void onCameraViewStopped() {
+
+  }
+
+  @Override
+  public Mat onCameraFrame(Mat inputFrame) {
+    return inputFrame;
+  }
+
+  public void onDestroy() {
+    super.onDestroy();
+    if (mOpenCvCameraView != null)
+      mOpenCvCameraView.disableView();
+  }
+
 }
